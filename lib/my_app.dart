ad6e09571f96469979d8d7d3d372b5fd5946af50
Samuel Silva <samuel7silva@outlook.com>
2023-10-09 17:16:45 -0300

    create theme color

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:task_flow/screens/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,22 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Task Flow',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        brightness: Brightness.light,
+        colorScheme: const ColorScheme.light(
+            background: Colors.white,
+            primary: Colors.amber,
+            secondary: Colors.amber),
+        primarySwatch: Colors.amber,
+        textTheme: GoogleFonts.outfitTextTheme(
+            const TextTheme(bodyMedium: TextStyle(color: Colors.black))),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Task Flow'),
